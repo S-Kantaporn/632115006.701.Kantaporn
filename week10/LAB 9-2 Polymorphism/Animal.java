f1fbf0faa4794e6f63aec346ba4d28ce2355c6cf
S-Kantaporn <77341542+S-Kantaporn@users.noreply.github.com>
2021-02-11 17:36:17 +0700

    Add files via upload

---
+++ week10/LAB 9-2 Polymorphism/Animal.java
@@ -0,0 +1,11 @@
+//Kantaporn Sriwichai
+//Student ID: 632115006
+//Sec 702 //LAB9.2
+
+package Week10;
+
+public class Animal {
+    public String roar() {
+        return "";
+    }
+}
---
+++ week10/LAB 9-2 Polymorphism/Cat.java
@@ -0,0 +1,12 @@
+//Kantaporn Sriwichai
+//Student ID: 632115006
+//Sec 702 //LAB9.2
+
+package Week10;
+
+public class Cat extends Animal {
+    public String roar() {
+        return "Meow Meow";
+    }
+    
+}
