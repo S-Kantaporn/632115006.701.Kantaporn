9409169735e1a3062ccc4742e19c9fb83572d525
S-Kantaporn <77341542+S-Kantaporn@users.noreply.github.com>
2021-03-30 21:36:16 +0700

    Add files via upload

---
+++ week12/LAB12 abstract/ArbitraryShape.java
@@ -0,0 +1,16 @@
+//Kantaporn Sriwichai
+//Student ID: 632115006
+//Lab12: Abstract
+
+package Lab12;
+
+public abstract class ArbitraryShape {
+
+
+    public ArbitraryShape() {
+
+    }
+
+    public abstract double getArea();
+
+}
