be18af8397e21d4cd32059952546e4cdbc662261
S-Kantaporn <77341542+S-Kantaporn@users.noreply.github.com>
2021-03-30 20:40:08 +0700

    Create TestCircle.java

---
+++ week4/LAB6/TestCircle.java
@@ -0,0 +1,25 @@
+//Kantaporn Sriwichai
+//632115006
+//LAB6 Encapsulation1
+
+package week4;
+
+public class TestCircle {
+    public static void main(String[] args) {
+        Circle myCircle = new Circle(5.0);
+        
+        System.out.println("The area of the circle of radius " + myCircle.getRadius() + " is " + myCircle.getArea());
+        myCircle.setRadius(myCircle.getRadius() *1.1);
+        System.out.println("The area of the circle of radius "+ myCircle.getRadius() + " is " + myCircle.getArea());
+        
+        //test#1
+        System.out.println(myCircle.getRadius());
+        //test#2
+        System.out.println(Circle.numberOfObjects);
+        //test#3
+        System.out.println("The number of object is " + myCircle.getNumberOfObjects());
+        //test#4
+        Circle.setNumberOfObjects(10);
+        System.out.println("The number of object is " + myCircle.getNumberOfObjects());
+    }
+}
